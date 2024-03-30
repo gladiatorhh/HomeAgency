f242f4cc5e08b48c4ecc488b3f1842502b7a1321
Arian Haghighi <arianhaghighi732@gmail.com>
2024-03-30 17:25:15 +0330

    Creaet add villa section and display villas

--- HomeAgency.Domain/Entities/Villa.cs
+++ HomeAgency.Domain/Entities/Villa.cs
@@ -1,19 +1,25 @@
-﻿namespace HomeAgency.Domain.Entities;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HomeAgency.Domain.Entities;
 
 public class Villa
 {
     public int Id { get; set; }
 
+    [MaxLength(100)]
     public required string Name { get; set; }
 
     public string? Description { get; set; }
 
     public double Price { get; set; }
 
+    [Range(1,10000)]
     public int Sqft { get; set; }
 
+    [Range(1, 10)]
     public int Occupancy { get; set; }
 
+    [Display(Name="Image url")]
     public string? ImageUrl { get; set; }
 
     public DateTime? CreateDate { get; set; }
---
+++ HomeAgency.Web/Controllers/VillaController.cs
@@ -0,0 +1,42 @@
+﻿using HomeAgency.Domain.Entities;
+using HomeAgency.Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HomeAgency.Web.Controllers;
+
+public class VillaController : Controller
+{
+
+    private readonly HomeAgencyDbContext _context;
+
+    public VillaController(HomeAgencyDbContext context)
+    {
+        _context = context;
+    }
+
+    public IActionResult Index() =>
+        View(_context.villas.ToList());
+
+    public IActionResult Create() =>
+        View();
+
+    [HttpPost]
+    public IActionResult Create(Villa obj)
+    {
+        if (obj.Name == obj.Description)
+        {
+            ModelState.AddModelError("", "Villa name shouldn't be the same as description");
+            return View(obj);
+        }
+
+        if (!ModelState.IsValid)
+        {
+            return View(obj);
+        }
+
+        _context.villas.Add(obj);
+        _context.SaveChanges();
+
+        return RedirectToAction(nameof(Index));
+    }
+}
