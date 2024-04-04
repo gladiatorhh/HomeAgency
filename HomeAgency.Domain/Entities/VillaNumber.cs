956c2dadd5af61a989376d8067d684401d9cb964
Arian Haghighi <arianhaghighi732@gmail.com>
2024-04-04 20:23:11 +0330

    Add villa number table

---
+++ HomeAgency.Domain/Entities/VillaNumber.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeAgency.Domain.Entities;
+
+public class VillaNumber
+{
+    [Key,DatabaseGenerated(DatabaseGeneratedOption.None)]
+    [Display(Name = "Villa number")]
+    public int Villa_Number { get; set; }
+
+    public string? Specialities { get; set; }
+
+    [ForeignKey(nameof(Villa))]
+    public int VillaId { get; set; }
+    public Villa Villa { get; set; }
+}
--- HomeAgency.Infrastructure/Data/HomeAgencyDbContext.cs
+++ HomeAgency.Infrastructure/Data/HomeAgencyDbContext.cs
@@ -7,17 +7,31 @@ public class HomeAgencyDbContext : DbContext
 {
     public HomeAgencyDbContext(DbContextOptions<HomeAgencyDbContext> options) : base(options)
     {
-        
+
     }
 
     public DbSet<Villa> villas { get; set; }
 
+    public DbSet<VillaNumber> VillaNumbers { get; set; }
+
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<Villa>().HasData(
-            new Villa { Id = 1,Name = "LasVegas Mantion",Description = "This is a great villa",Price = 99.9d},
-            new Villa { Id = 2,Name = "NewYork Mantion",Description = "This is a great villa",Price = 99.9d},
-            new Villa { Id = 3,Name = "LasVegas Mantion",Description = "This is a great villa",Price = 99.9d});
+            new Villa { Id = 1, Name = "LasVegas Mantion", Description = "This is a great villa", Price = 99.9d },
+            new Villa { Id = 2, Name = "NewYork Mantion", Description = "This is a great villa", Price = 99.9d },
+            new Villa { Id = 3, Name = "LasVegas Mantion", Description = "This is a great villa", Price = 99.9d });
+
+        modelBuilder.Entity<VillaNumber>().HasData(
+            new VillaNumber { Villa_Number = 101, VillaId = 1 },
+            new VillaNumber { Villa_Number = 102, VillaId = 1 },
+            new VillaNumber { Villa_Number = 103, VillaId = 1 },
+            new VillaNumber { Villa_Number = 201, VillaId = 2 },
+            new VillaNumber { Villa_Number = 202, VillaId = 2 },
+            new VillaNumber { Villa_Number = 203, VillaId = 2 },
+            new VillaNumber { Villa_Number = 301, VillaId = 3 },
+            new VillaNumber { Villa_Number = 302, VillaId = 3 },
+            new VillaNumber { Villa_Number = 303, VillaId = 3 });
+
 
         //base.OnModelCreating(modelBuilder);
     }
